46f9c390e356e2fb71efaffd6f49c3918c83da97
Adrian Irving-Beer <wisq@wisq.net>
2019-02-12 00:21:24 -0500

    Add proper Makefile support.

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
 MIX = mix
 CFLAGS += -g -O3 -std=c99 -Wall -Wextra -Wno-unused-parameter
 
-all: priv/reader compile
+all: priv/reader
 
-compile:
-	$(MIX) compile
+clean:
+	rm -f priv/reader
+
+.PHONY: all clean
 
 priv/reader: src/reader.c priv
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $<
--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ExCO2Mini.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      compilers: [:elixir_make] ++ Mix.compilers
     ]
   end
 
@@ -23,6 +24,7 @@ defmodule ExCO2Mini.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:elixir_make, "~> 0.4", runtime: false}
     ]
   end
 end
