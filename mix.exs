e585010225691ee8feccf2e5bf11a257a743c799
Adrian Irving-Beer <wisq@wisq.net>
2019-02-12 15:54:57 -0500

    Add more development dependencies.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ExCO2Mini.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      compilers: [:elixir_make] ++ Mix.compilers
+      compilers: [:elixir_make] ++ Mix.compilers()
     ]
   end
 
@@ -24,7 +24,9 @@ defmodule ExCO2Mini.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:elixir_make, "~> 0.4", runtime: false}
+      {:elixir_make, "~> 0.4", runtime: false},
+      {:ex_doc, "~> 0.10", only: :dev},
+      {:version_tasks, "~> 0.11.1", only: :dev}
     ]
   end
 end
