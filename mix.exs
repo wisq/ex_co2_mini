b949f6a13abcf27b78ec0cdeee16f746ab4b2360
Adrian Irving-Beer <wisq@wisq.net>
2019-02-12 22:02:44 -0500

    Bump to 0.1.3.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCO2Mini.MixProject do
   def project do
     [
       app: :ex_co2_mini,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
